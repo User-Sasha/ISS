0b919a2d184baf9f9fcaf7d109186892f7dc396f
sasha <sasha.laigle2022@campus-eni.fr>
2023-03-03 10:54:24 +0100

    Ajout liste astronautes + bulles

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-//Initialisation des variables Latitude et Longitude
+//Initialisation des données
+let nom = "iss";
+let infos;
+let altitude = 0;
+let vitesse = 0;
 let latitude = 48.85808672815468;
 let longitude = 2.2948842885382748;
+let textNaute = $("#textNaute");
+let astronaute = $("#austronautes");
 
 /*
  *  Initalisation de la map
@@ -45,31 +51,77 @@ setInterval(
             $("#issPosition").text("Latitude = "+latitude + " | " + "Longitude = " + longitude);
             map.panTo([latitude, longitude]);
             marker.setLatLng([latitude, longitude]);
+            //marker.bindTooltip(personnePresente).openTooltip();
+            //marker.bindTooltip(peopleInISS.toString()).openTooltip();
         })
 
-        $.ajax({
-            url: "http://api.open-notify.org/astros.json",
-            method: "GET",
-        }).done((donnees) => {
-            let peopleInSpace = donnees.people;
-            let peopleInISS = [];
-            for (let i = 0; i <peopleInSpace.length; i++) {
-                //$("#issPeople").text(people[i].craft);
-                if (peopleInSpace[i].craft === 'ISS'){
-                    peopleInISS.push(peopleInSpace[i].name);
-                }
-            }
-
-            let string = peopleInISS.toString()
-            let inside = string.replaceAll(',', '</br>')
-            let personnePresente = "Les personnes présentes dans l'iss actuellement sont : </br>" + inside
-            console.log(personnePresente);
-            marker.bindTooltip(personnePresente).openTooltip();
-           //marker.bindTooltip(peopleInISS.toString()).openTooltip();
-
-        })
 
     }, 1000);
+
+
+//Recupération des infos de l'iss
+$.ajax({
+    url: "https://api.wheretheiss.at/v1/satellites/25544",
+    method: "GET",
+}).done((donnees) => {
+    nom = donnees.name;
+    altitude = donnees.altitude;
+    vitesse = donnees.velocity;
+    map.panTo([latitude, longitude]);
+    marker.setLatLng([latitude, longitude]);
+    infos = "-Vaisseau : "+nom +"</br>-l'altitude est de : "+altitude + "</br>-la vitesse est de : "+vitesse+" km/h";
+
+    marker.bindTooltip(infos, {
+        interactive: true,
+        permanent: false});
+})
+
+
+
+function hideText() {
+    if (textNaute) {
+        for (var x = 0; x < textNaute.length; x++) {
+            textNaute[x].style.visibility = "hidden";
+        }
+    }
+};
+
+function showText(){
+    if (textNaute) {
+        for (var x = 0; x < textNaute.length; x++) {
+            textNaute[x].style.visibility = "visible";
+        }
+    }
+}
+
+function textPopup() {
+    $.ajax({
+        url: "http://api.open-notify.org/astros.json",
+        method: "GET",
+    }).done((donnees) => {
+        let peopleInSpace = donnees.people;
+        let peopleInISS = [];
+        for (let i = 0; i <peopleInSpace.length; i++) {
+            if (peopleInSpace[i].craft === 'ISS'){
+                peopleInISS.push(peopleInSpace[i].name);
+            }
+        }
+
+            console.log(astronaute);
+            astronaute[0].click(() => {
+                textNaute[0].style.visibility === 'visible' ? hideText() : showText();
+            })
+
+
+        /*
+        let string = peopleInISS.toString()
+        let inside = string.replaceAll(',', '\n')
+        let personnePresente = "Bonjour, je suis naute, \nLes personnes présentes dans l'iss actuellement sont : \n" + inside + "\n>" + "Naute";
+        $("#austronautes").text(personnePresente);
+        console.log(personnePresente);
+        */
+    })
+}
 
 /*
  *feature
@@ -80,6 +132,7 @@ setInterval(
  * https://stackoverflow.com/questions/36900540/moving-an-image-with-javascript-onclick
  *
  */
+
 
 
 
