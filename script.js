e796a19f7212494654ac41b95db0609f39157f17
sasha <sasha.laigle2022@campus-eni.fr>
2023-03-06 14:02:14 +0100

    integration konami

--- script.js
+++ script.js
@@ -101,6 +101,17 @@ astronaute[0].addEventListener('click', () => {
 })
 
 
+if ( window.addEventListener ) {
+    var state = 0, do_want = [38,38,40,40,37,39,37,39,66,65];
+    window.addEventListener(" keydown ", function(e) {
+        if ( e.keyCode == do_want[state] ) state++;
+        else state = 0;
+        if ( state == 10 )
+            window.location = " http://fr.playstation.com/ps4/ ";
+            }, true);
+}
+
+
 /*
  * Initalisation des particules
  */
